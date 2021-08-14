3df145a8a487f4b58ea0fb364573b519d1b85535
Guilherme Henrique Ribeiro <guilherme.h.ribeiro@bradesco.com.br>
2021-08-14 16:44:13 -0300

    list

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/07/08/07/23/child-6396101_960_720.png'),
+  '2': const User(
+      id: '2',
+      name: 'Rafael',
+      email: 'rafael@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png'),
+  '3': const User(
+      id: '3',
+      name: 'Fernanda',
+      email: 'fernanda@alunos.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_960_720.png'),
+  '4': const User(
+      id: '4',
+      name: 'Guilherme',
+      email: 'guilherme@xyz.com.br',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2017/01/31/21/22/avatar-2027363_960_720.png')
+};
